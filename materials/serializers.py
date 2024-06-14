ad2fa3af7d33ae3da87e9bcb00689735a2c210e2
ILKHOM <gaffarovilkhom@inbox.ru>
2024-06-14 17:59:53 +0300

    An additional check has been implemented to ensure that materials contain no links to third-party resources other than youtube.com.

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,10 +1,12 @@
+from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
-
+from materials.validators import youtube_url_validator
 from materials.models import Course, Lesson
 
 
 class LessonSerializer(ModelSerializer):
+    url_video = serializers.URLField(validators=[youtube_url_validator])
 
     class Meta:
         model = Lesson
---
+++ materials/validators.py
@@ -0,0 +1,6 @@
+from rest_framework.serializers import ValidationError
+
+
+def youtube_url_validator(value):
+    if 'https://www.youtube.com/' not in value.lower():
+        raise ValidationError('Использована запрещенная ссылка')
